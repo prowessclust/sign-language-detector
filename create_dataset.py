0a5e5ba453e103e214ab2f38a47718181e7aab0a
Priyanka Kumari <166229841+prowessclust@users.noreply.github.com>
2025-09-09 06:54:15 +0530

    Landmarks created

---
+++ create_dataset.py
@@ -0,0 +1,29 @@
+import os
+import cv2
+import mediapipe as mp
+import matplotlib.pyplot as plt
+
+DATA_DIR ='./data'
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+#write code for seeing images
+
+for dir_ in os.listdir(DATA_DIR):
+  for img_path in os.listdir(os.path.join(DATA_DIR, dir_))[:1]:
+    img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(img_rgb)
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        for i in range(len(hand_landmarks.landmark)):
+          print(hand_landmarks.landmark[i])
+    plt.figure()
+    plt.imshow(img_rgb)
+
+plt.show()
